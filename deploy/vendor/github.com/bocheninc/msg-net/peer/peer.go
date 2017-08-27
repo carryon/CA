b8d467937b0129e7e381c9f0a1399bd782afc2e7
yanchenxu <yanchenxu1993@163.com>
2017-08-27 11:00:46 +0800

    reconnect

--- deploy/vendor/github.com/bocheninc/msg-net/peer/peer.go
+++ deploy/vendor/github.com/bocheninc/msg-net/peer/peer.go
@@ -1,18 +1,18 @@
 // Copyright (C) 2017, Beijing Bochen Technology Co.,Ltd.  All rights reserved.
 //
-// This file is part of msg-net 
-// 
+// This file is part of msg-net
+//
 // The msg-net is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // The msg-net is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 
+//
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -117,7 +117,7 @@ func (p *Peer) Start() bool {
 					if d, err := time.ParseDuration(config.GetString("router.reconnect.interval")); err == nil {
 						duration = d
 					}
-					max := 5
+					max := 10000
 					if n := config.GetInt("router.reconnect.max"); n != 0 {
 						max = n
 					}
